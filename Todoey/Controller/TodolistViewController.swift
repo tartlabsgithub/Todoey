aee0202331f299608f777a249888a3e3444acc15
Tart Labs <tartlabs@Ashmitas-Mac-mini.local>
2018-04-27 15:13:15 +0530

    Persistent Data Storage was Successfully

--- Todoey/Controller/TodolistViewController.swift
+++ Todoey/Controller/TodolistViewController.swift
@@ -11,37 +11,37 @@ import UIKit
 class TodolistViewController: UITableViewController {
     
     var itemArray = [Item]()
-    let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let newItem = Item()
-        newItem.title = "Find Mike"
-        itemArray.append(newItem)
+        print(dataFilePath)
         
-        let newItem2 = Item()
-        newItem2.title = "Gowtham"
-        itemArray.append(newItem2)
+        //        let newItem = Item()
+        //        newItem.title = "Find Mike"
+        //        itemArray.append(newItem)
+        //
+        //        let newItem2 = Item()
+        //        newItem2.title = "Gowtham"
+        //        itemArray.append(newItem2)
+        //
+        //        let newItem3 = Item()
+        //        newItem3.title = "Tartlabs"
+        //        itemArray.append(newItem3)
         
-        let newItem3 = Item()
-        newItem3.title = "Tartlabs"
-        itemArray.append(newItem3)
+        loadItems()
         
-        if let item = defaults.object(forKey: "TodoItemArray") as? [Item] {
-          itemArray = item
-      }
     }
     
     //Mark TableView Datasource Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
-        
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-       let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         let item = itemArray[indexPath.row]
         cell.textLabel?.text = item.title
         cell.accessoryType = item.done ? .checkmark : .none
@@ -65,8 +65,7 @@ class TodolistViewController: UITableViewController {
             let newItem = Item()
             newItem.title = textField.text!
             self.itemArray.append(newItem)
-            self.defaults.set(self.itemArray, forKey: "TodoItemArray")
-            self.tableView.reloadData()
+            self.saveItems()
         }
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create New Item"
@@ -76,6 +75,28 @@ class TodolistViewController: UITableViewController {
         }
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+    }
+    
+    public func saveItems() {
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(self.itemArray)
+            try data.write(to:self.dataFilePath! )
+        }catch {
+            print("Error Encoding Item Array \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    
+    public func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            }catch {
+                print("Error decoding Arrar \(error)")
+            }
+        }
     }
     
 }
